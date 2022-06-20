d4bfd622401e4e04bf9437140f56c8835f09dba7
JangWonJong <jwj9922@naver.com>
2022-06-20 20:46:16 +0900

    reduce_mean

--- model/auto_encoder.py
+++ model/auto_encoder.py
@@ -1,36 +1,40 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+
 class Solution:
     def __init__(self) -> None:
-        import tensorflow as tf
-        import numpy as np
-        import matplotlib.pyplot as plt
-
-        from tensorflow.examples.tutorials.mnist import input_data
-
-        mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
         # ******
         # 옵션 설정
         # ******
-        learning_rate = 0.01
-        training_epoch = 20
-        batch_size = 100
+        self.learning_rate = 0.01
+        self.training_epoch = 20
+        self.batch_size = 100
         # 신경망 레이어 구성옵션
-        n_hidden = 256 # 히든 레이어의 뉴런 갯수
-        n_input = 28 * 28
+        self.n_hidden = 256 # 히든 레이어의 뉴런 갯수
+        self.n_input = 28 * 28
+
+    def create(self):
+
         # ******
         # 신경망 모델 구성
         # ******
-        X = tf.placeholder(tf.float32, [None, n_input])
+        X = tf.placeholder(tf.float32, [None, self.n_input])
         # Y 는 선언하지 않음. 입력값을 Y 로 사용하기 때문
 
-        W_encode = tf.Variable(tf.random_normal([n_input, n_hidden]))
-        b_encode = tf.Variable(tf.random_normal([n_hidden]))
+        W_encode = tf.Variable(tf.random_normal([self.n_input, self.n_hidden]))
+        b_encode = tf.Variable(tf.random_normal([self.n_hidden]))
         # 인코더 레이어와 디코더 레이어의 가중치와 편향변수를 설정
 
         encoder = tf.nn.sigmoid(tf.add(tf.matmul(X, W_encode), b_encode))
 
-        W_decode = tf.Variable(tf.random_normal([n_hidden, n_input]))
-        b_decode = tf.Variable(tf.random_normal([n_input]))
+        W_decode = tf.Variable(tf.random_normal([self.n_hidden, self.n_input]))
+        b_decode = tf.Variable(tf.random_normal([self.n_input]))
         # 디코더 레이어 구성. 이 디코더가 최종 모델이 됨
         decoder = tf.nn.sigmoid(tf.add(tf.matmul(encoder, W_decode), b_decode))
 
@@ -39,7 +43,7 @@ class Solution:
         # 차이를 손실값으로 설정합니다.
 
         cost = tf.reduce_mean(tf.pow(X - decoder, 2))
-        optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+        optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(cost)
 
         # ******
         # 신경망 모델 학습
